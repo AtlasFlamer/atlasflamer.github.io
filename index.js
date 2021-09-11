0d194d29f7af65c909e76cbf3d3f417793b9713d
benja robles <benjaa.robless@gmail.com>
2021-09-10 21:51:57 -0300

    branch init

--- index.js
+++
@@ -1,4 +0,0 @@
-function registerButton(){
-	console.log("uwu");
-}
-
--- sample.js
+++
@@ -1,5 +0,0 @@
-const content = 'http://asciidoctor.org[*Asciidoctor*] ' +
-  'running on https://opalrb.com[_Opal_] ' +
-  'brings AsciiDoc to Node.js!'
-const html = convert(content) // (2)
-console.log(html) // (3)
